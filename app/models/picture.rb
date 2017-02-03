d6d74131e25c74a563c5acd1df81bd76435c3b56
sylvia villegas <sylvia@svillegas.com>
2017-02-02 21:12:57 -0500

    Add validation for model Picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,10 @@
 class Picture < ApplicationRecord
+
+  validates :artist, presence: :true
+
+  validates :title, length: { within: 3..20 }
+
+  validates :url, presence: :true,
+                  uniqueness: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Rails.application.routes.draw do
 
   resources :pictures
 
+  # get "pictures" => "pictures#index"
+  #
+  # post "pictures" => "pictures#create"
+  # get "pictures/new" => "pictures#new"
+  #
+  # get "pictures/:id" => "pictures#show", as: "picture"
+  #
+  # get "pictures/:id/edit" => "pictures#edit", as: "edit_picture"
+  # patch "pictures/:id" => "pictures#update"
+  #
+  # delete "pictures/:id" => "pictures#destroy", as: "delete_picture"
 end
